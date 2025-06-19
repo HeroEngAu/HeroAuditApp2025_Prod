79ec86eb332bf405f5dd4eff0e210a0953839d80
leonardocunha13 <leonardo.dejesus@heroengineering.com.au>
2025-06-19 14:32:40 +0800

    adjustments on the files to make it run smooth in the android

--- app/head.tsx
+++ app/head.tsx
@@ -4,7 +4,7 @@ export default function Head() {
       <title>HeroAudit App</title>
       <meta name="theme-color" content="#facc15" />
       <link rel="manifest" href="/manifest.json" />
-      <link rel="apple-touch-icon" href="/icons/icon-192.png" />
+      <link rel="apple-touch-icon" href="/icon-192x192.png" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
     </>
   );
